e62442f7652f815f2e6b3b75d4cb3b83466118f0
PamanAleph <alieffirmandabisnis@gmail.com>
2024-10-13 11:17:45 +0700

    "chore: Updated ButtonBar component styles: removed border and px-[2vw] from section, added gap-[4.5rem] to section, and added border-[#262626] to buttons."

--- src/components/friends/ButtonBar.tsx
+++ src/components/friends/ButtonBar.tsx
@@ -9,36 +9,36 @@ interface ButtonBarProps {
 
 export default function ButtonBar({ user }: ButtonBarProps) {
   return (
-    <section className="flex items-center justify-between px-[2vw] border border-[#0F0F0F] rounded-xl text-[1.5vw] text-[#808080]">
+    <section className="flex items-center justify-between gap-[4.5rem] rounded-xl text-[1.5vw] text-[#808080]">
       {/* Prev Button */}
-      <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw]">
+      <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw] border-[#262626]">
         <div className="flex">
-          <ArrowLeftIcon color="white"/>
+          <ArrowLeftIcon color="white" />
           <p>Prev</p>
         </div>
       </button>
 
       {/* Links */}
-      <div className="flex space-x-[2vw] rounded-[2vw] p-[1vw]">
+      <div className="flex space-x-[2vw] rounded-[2vw] p-[1vw] border border-[#262626]">
         <Link href={`friends/book/online/${user.id}`}>
-          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw]">
+          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw] border-[#262626]">
             Online Date
           </button>
         </Link>
         <Link href={`friends/chat/${user.id}`}>
-          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-white text-[1vw]">
+          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-white text-[1vw] border-[#262626]">
             Say Hi👏
           </button>
         </Link>
         <Link href={`friends/book/offline/${user.id}`}>
-          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw]">
+          <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw] border-[#262626]">
             Offline Date
           </button>
         </Link>
       </div>
 
       {/* Next Button */}
-      <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw]">
+      <button className="border px-[2vw] py-[1.5vh] rounded-[2vw] text-[1vw] border-[#262626]">
         Next
       </button>
     </section>
