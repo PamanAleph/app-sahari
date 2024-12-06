d012e94f2698640251a6549494c4b349a24e2de5
PamanAleph <alieffirmandabisnis@gmail.com>
2024-12-06 20:09:59 +0700

    chore: Refactor auth components: rename LoginForm and RegisterForm to login/LoginForm and register/RegisterForm, delete ForgotPasswordForm and MFAForm

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import Hero from "@/assets/background/Hero.png";
-import LoginForm from "@/components/auth/LoginForm";
+import LoginForm from "@/components/auth/login/LoginForm";
 
 export default function page() {
   return (
--- src/app/(auth)/auth/register/page.tsx
+++ src/app/(auth)/auth/register/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import Hero from "@/assets/background/Hero.png";
-import RegisterForm from "@/components/auth/RegisterForm";
+import RegisterForm from "@/components/auth/register/RegisterForm";
 
 export default function page() {
   return (
